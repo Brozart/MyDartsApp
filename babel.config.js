758ce2b26e1c0f6360cd1f458f5915212e864f60
Tom Van den Broeck <tom-vdbroeck@hotmail.com>
2023-05-13 22:37:01 +0200

    Module config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin', ['module-resolver']],
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          alias: {
+            common: './app/common',
+            content: './app/content',
+            domain: './app/domain',
+            navigation: './app/navigation',
+            store: './app/store',
+          },
+        },
+      ],
+    ],
   };
 };
